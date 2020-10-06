31603748175727eea6e659d7a0251ecc8fa268ec
Gabriela Pivetta <gpivetta99@gmail.com>
2020-10-06 17:26:33 -0400

    style(#9): Styled login page using react-bootstrap and next.js
    Co-authored-by: Giuulob89 <giulialobo89@gmail.com>

--- ziguen/pages/iniciarsessao.js
+++ ziguen/pages/iniciarsessao.js
@@ -1,33 +1,39 @@
-import Navbar from '../components/navbar'
-import Slider from '../components/slider'
+import Navbar from '../components/navbar-login'
 import Footer from '../components/footer'
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 
 export default function Iniciarsessao() {
   return (
     <div>
       <Navbar />
-      <div className="search-bar">
-        <div className="container">
-          <div>
-          <h3>Login</h3>
-            <div className="row">
-              <div className="form-group col-md-3">
-                <label>Login</label>
-                <input type="text" name="name" className="form-control" />
-              </div>
-              <div className="form-group col-md-3">
-                <label>Senha</label>
-                <input type="text" name="name" className="form-control" />
-              </div>
-              <div className="form-group col-md-3">
-                <label>&nbsp;</label>
-                <button className="btn btn-primary d-block">Logar</button>
-              </div>     
-            </div>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <h3 className="text-center login-titulo">Login</h3>
+        </Row>
+
+        <Row>
+          <Col sm={3}></Col>
+          <Col sm={4}>
+              <Form className="login-form">
+                <Form.Group controlId="formGroupEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="secondary" size="lg">Entrar</Button>
+              </Form>
+          </Col>
+          <Col sm={5}></Col>
+        </Row>
+      </Container>
+      <Footer/>
     </div>
   )
 }
