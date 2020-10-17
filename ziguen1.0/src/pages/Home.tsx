b104551d51e0b197a7f18893302ed169da6b2189
Gabriela Pivetta <gpivetta99@gmail.com>
2020-10-17 19:45:00 -0400

    style(#18): creating home and login pages
    Co-authored-by: GeiseSaunier <geisesaunier3939@gmail.com>

---
+++ ziguen1.0/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div id="home-page">
+            <header>
+                <h1>Hello World</h1>
+            </header>
+        </div>
+
+    )
+}
+
+export default Home;
---
+++ ziguen1.0/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div id="login-page">
+            <header>
+                <h1>Hello Login</h1>
+            </header>
+        </div>
+
+    )
+}
+
+export default Login;
