9fd9f50d6d9f5b6302198cea6f87ec56c4bc1cc5
Gabriela Pivetta <gpivetta99@gmail.com>
2020-10-17 20:33:06 -0400

    feat(#20): creating header
    Co-authored-by: GeiseSaunier <geisesaunier3939@gmail.com>

--- ziguen1.0/src/pages/Login.tsx
+++ ziguen1.0/src/pages/Login.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logoImg from '../images/logo.png';
+
+import '../styles/pages/login.css'
 
 function Login() {
     return (
         <div id="login-page">
-            <header>
-                <h1>Hello Login</h1>
-            </header>
+            <div className="top-bar">
+                <header>
+                    <img src={logoImg} alt="Logo" className="img-logo"/>
+
+                    <div className="navbar">
+                        <Link to="/" className="home-link">Página Inicial</Link>
+                        <Link to="/" className="home-link">Página Inicial</Link>
+                        <Link to="/" className="home-link">Página Inicial</Link>
+                    </div>
+                </header>
+                <div className="field">Passagens Fluviais</div>
+            </div>
         </div>
 
     )
