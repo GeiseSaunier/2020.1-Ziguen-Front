916a75d8eb4e12bdc3e36496e4050b1007f2230c
Giuulob89 <giulialobo89@gmail.com>
2020-11-11 20:36:52 -0300

    feat(#34): Doing the payment options screen
    Co-authored-by: liversonp <liverson.p@gmail.com>

--- src/pages/Opcoes-de-pagamento.tsx
+++ src/pages/Opcoes-de-pagamento.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../images/logo.png';
 import facebookImg from '../images/facebook.svg'
 import instagramImg from '../images/instagram.svg'
+import cartaoImg from '../images/cartao-de-credito.png'
+import boletoImg from '../images/boleto-bancario.png'
 
 import '../styles/pages/opcoes-de-pagamento.css'
 import '../styles/pages/header.css'
@@ -25,8 +27,31 @@ function Payment() {
                 </div>
             </div>
 
-            <div className="pagamento">
+            <h1>Formas de Pagamento</h1>
+
+            <div className="formas-de-pagamento">
+                <div className = "cartao-de-credito">
+                    <div>
+                        <input type = 'radio' name = "pagamento"/>
+                        <label htmlFor=""> Cartão de Crédito</label>
+                    </div>
+                    <img src = {cartaoImg} alt="cartao"/>
+                    <select name="Escolha o cartão que deseja utilizar" id="escolha-cartao">
+                        <option value="1">Cartão A</option>
+                        <option value="2">Cartão B</option>
+                    </select>
+                    <button>Cadastrar novo cartão</button>
+                    <button>Revisar Dados e Finalizar Compra</button>
+                </div>
                 <div className="division-bar"></div>
+                <div className = "boleto-bancario">
+                   <div>
+                        <input type = 'radio' name = "pagamento"/>
+                        <label htmlFor=""> Boleto Bancário</label>
+                    </div>
+                    <img src={boletoImg} alt="boleto"/>
+                    <button>Gerar Boleto e Finalizar Compra</button>
+                </div>
             </div>
 
             <div className="footer">
