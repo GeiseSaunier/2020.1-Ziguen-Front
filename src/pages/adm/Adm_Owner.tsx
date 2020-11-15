3edf0ac8905a5ebbe5685bde0f1ed8845bb17aa5
Gabriela Pivetta <gpivetta99@gmail.com>
2020-11-15 15:52:34 -0300

    style(#28): organizing owners page
    Co-authored-by: GeiseSaunier <geisesaunier3939@gmail.com>

--- src/pages/adm/Adm_Owner.tsx
+++ src/pages/adm/Adm_Owner.tsx
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../images/logo.png';
 import facebookImg from '../../images/facebook.svg'
 import instagramImg from '../../images/instagram.svg'
-
-import tickets from '../../images/printer.png'
 import lixo from '../../images/trash-can.png'
 import lapis from '../../images/pencil.png'
-
-
 import lupa from '../../images/lupa.jpg'
 
+import '../../styles/pages/adm/adm_owner.css'
 
 function Adm_Owner() {
     return (
@@ -31,23 +28,22 @@ function Adm_Owner() {
              {/*--------------- Mensagem ao Proprietário ---------------*/}
             <div className="saudacao">
                 <h1>Proprietários</h1>
-            </div>
+            
                 <form className="search-bar" action=" " method="post">
                     <input type="search" id="busca" name="q" />
                     <button type="submit"><img src={lupa} alt="Lupa"/></button>
                 </form>
-
+            </div>
                  {/*--------------- Especificações do Proprietário ---------------*/}
-                <div className="boats">
-                <div className="boat">
-                    <div className="num_identification">
+            <div className="owners">
+                <div className="owner">
+                    <div className="owner_name">
                         <strong>Nome do proprietário</strong>
                     </div>
         
-                    <strong>Tamanho da frota: 45 </strong>
+                    <strong>Tamanho da frota: <p>45</p></strong>
 
                     <div className="buttons_boat">
-                        <button className="boat-trips-button" type="button"><img src={tickets} alt="Viagens"/></button>
                         <button className="delete-boat-button" type="button"><img src={lixo} alt="Deletar"/></button>
                         <button className="edit-boat-button" type="button"><img src={lapis} alt="Editar"/></button>
                     </div>
