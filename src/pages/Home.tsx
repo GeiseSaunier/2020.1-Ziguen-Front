2df43b7b76406fd9f45ee38000a992944eb1aa91
liversonp <liverson.p@gmail.com>
2020-11-18 19:27:01 -0300

    fix(#34): Adjusting header
    Co-authored-by: Giuulob89 <giulialobo89@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,13 +18,16 @@ function Home() {
     return (
         <div id="home-page">
             {/*--------------- Top Bar ---------------*/}
-            <div className="top-bar">
-                <img src={logoImg} alt="Logo" className="img-logo" />
-                <div className="navbar">
-                    <Link to="/" className="mais-vendidas-link">Mais Vendidas</Link>
-                    <Link to="./Login" className="login-link">Iniciar Sessão</Link>
-                    <Link to="/" className="sobre-link">Sobre</Link>
-                    <Link to="/" className="ajuda-link">Ajuda</Link>
+           <div className="top-bar">
+                <div className = "header">
+                    <img src={logoImg} alt="Logo" className="img-logo" />
+                    <div className="navbar">
+                        <Link to="/" className="mais-vendidas-link">Mais Vendidas</Link>
+                        <Link to="./Login" className="login-link">Iniciar Sessão</Link>
+                        <Link to="/" className="perfil-link">Meu perfil</Link>
+                        <Link to="/" className="sobre-link">Sobre</Link>
+                        <Link to="/" className="ajuda-link">Ajuda</Link>
+                    </div>
                 </div>
                 <div className="field">
                     <b><p>PASSAGENS FLUVIAIS</p></b>
