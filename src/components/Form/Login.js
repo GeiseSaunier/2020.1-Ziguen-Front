db3295c5d8a136abe33f9500d284dc7858317596
francisco1code <francisco170142329@gmail.com>
2020-12-06 11:10:05 -0300

    feat(#31): Login request
    Co-authored-by: gabrielapivetta <gpivetta99@gmail.com>
    Co-authored-by: GeiseSaunier <geisesaunier3939@gmail.com>

--- src/components/Form/Login.js
+++ src/components/Form/Login.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from '@unform/web';
 import '../../styles/pages/login-registration-form.css';
+import { useHistory } from 'react-router-dom';
+
 
 import Input from '../../components/Form/input';
+import api from '../../services/api';
 
 
 export default function Login() {
-    function handleSubmit(data) {
-        console.log(data);
+
+    const history = useHistory();
+
+    async function handleSubmit(data) {
+        
+        const travelerData = {
+            email:data.email,
+            password:data.password
+        }
+        
+        try{
+            const response =  await api.post('/sessions/trav', travelerData); 
+            console.log(response)
+            history.push('/home-cliente')
+            alert('Longin feito com sucesso');
+        }catch(err){
+            alert('Email ou senha incorretos.');
+        }
+
     }
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
 
@@ -19,8 +42,8 @@ export default function Login() {
             <div id="form">
                 <h1>Login</h1><br></br>
 
-                <Input className="input-box" type="email" name="email" placeholder="   E-mail"/><br></br>
-                <Input className="input-box" type="password" name="password" placeholder="   Senha"/><br></br>
+                <Input className="input-box" type="email" value= {email} onChange={event => setEmail(event.target.value) }  name="email" placeholder="   E-mail"/><br></br>
+                <Input className="input-box" type="password" value= {password} onChange={event => setPassword(event.target.value) }  name="password" placeholder="   Senha"/><br></br>
 
                 <button className="login-button" type="submit">Entrar</button><br></br>
 
